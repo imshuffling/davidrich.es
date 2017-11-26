a5bc785d838025f39e6e6c9722ee9dbd7e218871
David Riches <hi@davidrich.es>
2017-11-26 14:57:47 +0000

    updating 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
+  <section id='page-not-found'>
+    <h1>404 - Sorry page no found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  </section>
 )
 
 export default NotFoundPage
