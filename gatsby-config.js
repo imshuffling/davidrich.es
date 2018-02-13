ecbb068fc33ea0a9673a4f6ca0d93d0e68af1ef0
David Riches <hi@davidrich.es>
2018-02-13 16:37:58 +0000

    more updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,24 +11,24 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "David Riches 🏑",
-        short_name: "David Riches 🏑",
+        short_name: "DR 🏑",
         start_url: "/",
         background_color: "#ffffff",
         theme_color: "#A864A8",
         display: "minimal-ui",
         icons: [
           {
-            src: `android-chrome-192x192.png`,
+            src: `/static/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `android-chrome-384x384.png`,
+            src: `/static/android-chrome-384x384.png`,
             sizes: `384x384`,
             type: `image/png`,
           },
           {
-            src: `android-chrome-512x512.png`,
+            src: `/static/android-chrome-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
