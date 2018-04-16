0577f9222ccce9d55d751e6b33c730c288041e29
David Riches <hi@davidrich.es>
2018-04-16 11:22:54 +0100

    :construction: updating path on thanks page, removing slash

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -58,7 +58,7 @@ export default class Contact extends React.Component {
           <form
             name="contact"
             method="post"
-            action="/thanks/"
+            action="/thanks"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
             onSubmit={this.handleSubmit}
