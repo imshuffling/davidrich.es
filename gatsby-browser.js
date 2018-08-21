bfb313f9ea84042bea2c6357e0fdf54a3f58d230
David Riches <hi@davidrich.es>
2018-08-21 15:38:12 +0100

    changing logo font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
-require('typeface-karla')
+require('typeface-karla');
+require('typeface-oswald');
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/index.scss'
 import 'typeface-karla'
+import 'typeface-oswald'
+
 
 const Layout = ({ children }) => (
   <section className="container-wrap">
