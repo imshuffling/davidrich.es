53871aa367839a88acd1aa6dfeb46a138ecd3eb2
David Riches <hi@davidrich.es>
2018-08-28 12:39:06 +0100

    adding exact to link router

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 const Header = () => (
   <header>
     <ul id="navigation">
-      <li><Link to='/' activeClassName="active"><span>About me</span></Link></li>
+      <li><Link exact to='/' activeClassName="active"><span>About me</span></Link></li>
       <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
       <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
     </ul>
