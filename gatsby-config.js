34133b1f940d8b6a18599cee89e1bd5c66f1b239
David Riches <hi@davidrich.es>
2018-08-31 08:35:31 +0200

    adding SRI

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,24 +33,30 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-      {
-        resolve: `gatsby-source-contentful`,
-          options: {
-            spaceId: `anjlutb8dq3v`,
-            accessToken: `4bf9314dc5bdfbf380247aa3ab84d7da0e86d33a6089168eb32558d4f7096cda`
-          },
-      },
-      {
-        resolve: `gatsby-plugin-netlify`,
-          options: {
-            headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-            allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-            mergeSecurityHeaders: true, // boolean to turn off the default security headers
-            mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-            mergeCachingHeaders: true, // boolean to turn off the default caching headers
-            transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-            generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+    {
+      resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: `anjlutb8dq3v`,
+          accessToken: `4bf9314dc5bdfbf380247aa3ab84d7da0e86d33a6089168eb32558d4f7096cda`
         },
+    },
+    {
+      resolve: 'gatsby-plugin-sri',
+      options: {
+        hash: 'sha512' // 'sha256', 'sha384' or 'sha512' ('sha512' = default)
+      }
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+        options: {
+          headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+          allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+          mergeSecurityHeaders: true, // boolean to turn off the default security headers
+          mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+          mergeCachingHeaders: true, // boolean to turn off the default caching headers
+          transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+          generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
+    },
   ],
 }
