a831071e92adef889bd78e4b01c03bf20294d612
David Riches <hi@davidrich.es>
2018-09-04 10:47:24 +0100

    fixing active style class

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// this is only active when the location pathname is exactly
+// the same as the href.
+const isActive = ({ isCurrent }) => {
+  return isCurrent ? { className: "active" } : null
+}
+
 const Header = () => (
   <header>
     <ul id="navigation">
-      <li><Link exact to='/' activeClassName="active"><span>About me</span></Link></li>
-      <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
-      <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
+      <li><Link to='/' getProps={isActive}><span>About me</span></Link></li>
+      <li><Link to='/services' getProps={isActive}><span>Services</span></Link></li>
+      <li><Link to='/contact' getProps={isActive}><span>Contact</span></Link></li>
     </ul>
     <div id="logo"><Link to='/'>David Riches</Link></div>
   </header>
