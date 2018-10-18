a898a4b0e45b1ed276286c46e8a36ee289a6de63
David Riches <hi@davidrich.es>
2018-10-18 11:30:54 +0100

    reverting back to link in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,20 @@
 import React from "react"
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 // this is only active when the location pathname is exactly
 // the same as the href.
-
-// <li><Link to='/' getProps={isActive}><span>About me</span></Link></li>
-// <li><Link to='/services' getProps={isActive}><span>Services</span></Link></li>
-// <li><Link to='/contact' getProps={isActive}><span>Contact</span></Link></li>
-//
-//
-// const isActive = ({ isCurrent }) => {
-//   return isCurrent ? { className: "active" } : null
-// }
+const isActive = ({ isCurrent }) => {
+  return isCurrent ? { className: "active" } : null
+}
 
 const Header = () => (
   <header>
     <ul id="navigation">
-      <li><a href="/"><span>About me</span></a></li>
-      <li><a href="/services"><span>Services</span></a></li>
-      <li><a href="/contact"><span>Contact</span></a></li>
+      <li><Link to='/' getProps={isActive}><span>About me</span></Link></li>
+      <li><Link to='/services' getProps={isActive}><span>Services</span></Link></li>
+      <li><Link to='/contact' getProps={isActive}><span>Contact</span></Link></li>
     </ul>
-    <div id="logo"><a href="/">David Riches</a></div>
+    <div id="logo"><Link to='/'>David Riches</Link></div>
   </header>
 )
 
