1be998f22bc8cc710288cfd1e841698f812bcb78
David Riches <hi@davidrich.es>
2018-11-02 20:35:14 +0000

    adding react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Img from "gatsby-image"
 import { graphql } from "gatsby"
 import Layout from "../components/layout.js"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-//import Helmet from 'react-helmet'
+import Helmet from 'react-helmet'
 
 export default (props) => (
   <Layout>
