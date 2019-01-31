e0b5a3d044554d85e88f3ca28926c4ead428681d
David Riches <hi@davidrich.es>
2019-01-31 11:55:05 +0000

    adding funky button bits

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import Header from '../components/header'
 import Footer from '../components/footer'
@@ -7,19 +7,27 @@ import 'typeface-karla'
 import 'typeface-oswald'
 
 
-const Layout = ({ children }) => (
-  <section className="container-wrap">
-    <Helmet
-      title="About me - David Riches"
-      meta={[
-        { name: 'description', content: 'Personal portfolio for David Riches.' },
-        { name: 'keywords', content: 'Front-end developer London, Drupal front-end developer, Drupal developer london' },
-      ]}
-    />
-    <Header />
-      {children}
-    <Footer />
-  </section>
-)
+class Layout extends Component {
+  componentWillUnmount () {
+    //
+  }
+  render() {
+    const { children } = this.props
+    return (
+      <section className="container-wrap">
+        <Helmet
+          title="About me - David Riches"
+          meta={[
+            { name: 'description', content: 'Personal portfolio for David Riches.' },
+            { name: 'keywords', content: 'Front-end developer London, Drupal front-end developer, Drupal developer london' },
+          ]}
+        />
+        <Header />
+          {children}
+        <Footer />
+      </section>
+    )
+  }
+}
 
 export default Layout
