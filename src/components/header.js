7707717d2919cf8713a0f1f79981ef0f0f574d35
David Riches <hi@davidrich.es>
2019-07-05 10:48:57 +0100

    updating mobile component

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,81 @@
-import React from "react"
+import React, { Component } from "react"
+import Media from 'react-media';
 import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
 
-const Header = () => (
-  <header>
-    <ul id="navigation">
-      <li><Link to='/' activeClassName="active"><span>About me</span></Link></li>
-      <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
-      <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
-    </ul>
-    <div id="logo"><Link to='/'>David Riches</Link></div>
-  </header>
-)
+// const Header = () => (
+//   <header>
+//     <ul id="navigation">
+//       <li><Link to='/' activeClassName="active"><span>About me</span></Link></li>
+//       <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
+//       <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
+//     </ul>
+//     <div id="logo"><Link to='/'>David Riches</Link></div>
+//   </header>
+// )
+
+class Header extends Component {
+
+  state = {
+    toggle: false
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
+  render() {
+    //const props = this.props
+    return (
+      <>
+      <Helmet>
+        <body className={this.state.toggle ? 'block' : '' } />
+      </Helmet>
+      <header className={this.state.toggle ? 'open' : '' }>
+        <Media query="(max-width: 479px)">
+          {matches =>
+            matches ? (
+              <>
+                {/* Mobile */}
+                <div className="mobile-nav">
+                  <div id="logo"><Link to='/'>David Riches</Link></div>
+                  <button className={this.state.toggle ? 'navbutton active' : 'navbutton' } onClick={this.toggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </button>
+                </div>
+                {this.state.toggle &&
+                  <nav>
+                    <ul id="navigation">
+                      <li><Link to='/' activeClassName="active"><span>About me</span></Link></li>
+                      <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
+                      <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
+                    </ul>
+                  </nav>
+                }
+              </>
+            ) : (
+              // Desktop
+              <>
+                <div id="logo"><Link to='/'>David Riches</Link></div>
+                <nav>
+                  <ul id="navigation">
+                    <li><Link to='/' activeClassName="active"><span>About me</span></Link></li>
+                    <li><Link to='/services' activeClassName="active"><span>Services</span></Link></li>
+                    <li><Link to='/contact' activeClassName="active"><span>Contact</span></Link></li>
+                  </ul>
+                </nav>
+              </>
+            )
+          }
+        </Media>
+      </header>
+      </>
+    )
+  }
+}
 
 export default Header
