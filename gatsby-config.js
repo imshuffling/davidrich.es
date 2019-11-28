96b98951df96031cf6e885eaa22109b390e0aac7
David Riches <hi@davidrich.es>
2019-11-28 15:22:43 +0000

    font preload snizzle

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-preload-fonts`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
