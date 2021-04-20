12ccfd024a97fb7bf00135e3a44b79b1eb509104
David Riches <david.riches@appnovation.com>
2021-04-20 15:33:57 +0100

    manifest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `David Riches - A Front-end developer & part-time hockey player from London.`,
-    siteUrl: `https://davidrich.es`
+    siteUrl: `https://davidrich.es`,
   },
   plugins: [
     {
@@ -15,8 +15,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        exclude: [`/thanks/`]
-      }
+        exclude: [`/thanks/`],
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
@@ -36,34 +36,40 @@ module.exports = {
         theme_color: "#F40088",
         display: "minimal-ui",
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon_options: {
+          // For all the options available, please see:
+          // https://developer.mozilla.org/en-US/docs/Web/Manifest
+          // https://w3c.github.io/manifest/#purpose-member
+          purpose: `maskable`,
+        },
       },
     },
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: `anjlutb8dq3v`,
-          accessToken: `4bf9314dc5bdfbf380247aa3ab84d7da0e86d33a6089168eb32558d4f7096cda`,
-          // downloadLocal: true,
-        },
+      options: {
+        spaceId: `anjlutb8dq3v`,
+        accessToken: `4bf9314dc5bdfbf380247aa3ab84d7da0e86d33a6089168eb32558d4f7096cda`,
+        // downloadLocal: true,
+      },
     },
     {
-      resolve: 'gatsby-plugin-sri',
+      resolve: "gatsby-plugin-sri",
       options: {
-        hash: 'sha512' // 'sha256', 'sha384' or 'sha512' ('sha512' = default)
-      }
+        hash: "sha512", // 'sha256', 'sha384' or 'sha512' ('sha512' = default)
+      },
     },
     {
       resolve: `gatsby-plugin-netlify`,
-        options: {
-          headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-          allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-          mergeSecurityHeaders: true, // boolean to turn off the default security headers
-          mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-          mergeCachingHeaders: true, // boolean to turn off the default caching headers
-          transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-          generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
     },
   ],
-}
+};
