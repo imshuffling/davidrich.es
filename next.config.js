857d92ab64495ccfffa45359da3330e712b2f294
David Riches <david.riches@appnovation.com>
2022-02-23 17:25:52 +0000

    adding service worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net']
   },
-}
+  pwa: {
+    dest: 'public'
+  }
+})
