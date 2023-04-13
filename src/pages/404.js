f83330a4dcedb4fd8a29f460684206b46bfb495f
David Riches <hi@davidrich.es>
2023-04-13 20:58:15 +0100

    404 bits n bobs

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
+import { Link } from "gatsby";
 
 const NotFound = () => {
   return (
     <Layout>
       <Helmet>
-        <title>404 - David Riches</title>
+        <title>Whoops 404 - David Riches</title>
       </Helmet>
       <section id="page-not-found">
-        <h1>404 - Sorry page no found.</h1>
+        <h1>Sorry page not found 😢</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link to="/">Lets go back to the homepage</Link>
       </section>
     </Layout>
   );
