e3a5ee36792c29cd19ecbdc41204cc01cb930641
David Riches <hi@davidrich.es>
2023-04-13 20:47:10 +0100

    fixes for netlify deploys

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,16 @@ module.exports = {
       },
     },
     `@skagami/gatsby-plugin-dark-mode`,
-    // `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 };
