c92ed3c5df147a4753924e2d5f6577f323129263
David Riches <hi@davidrich.es>
2024-08-19 15:02:25 +0100

    netlify snizzle

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-adapter-netlify`,
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
@@ -67,17 +68,6 @@ module.exports = {
       },
     },
     `@skagami/gatsby-plugin-dark-mode`,
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
