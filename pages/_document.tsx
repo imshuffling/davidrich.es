a9e5f88144683ee540daf8bd2719606f2c2f7b3b
David Riches <hi@davidrich.es>
2024-12-16 16:41:40 +0000

    adding manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link rel="manifest" href="/static/manifest.webmanifest" />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
