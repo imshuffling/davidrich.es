234f016600070bd745ba82c5e28bf423d50024f9
David Riches <hi@davidrich.es>
2025-07-22 20:35:49 +0100

    update

--- blocks/blockimage/index.js
+++ blocks/blockimage/index.js
@@ -1,14 +1,30 @@
 import Image from "next/image";
 import imageBlur from "../../utils/imageBlur";
 
-export default function BlockImage({ image, lazyLoad }) {
+export default function BlockImage({ image, lazyLoad, aspectRatio = "16/9" }) {
+  const ratioMap = {
+    "16/9": { ratio: 16 / 9, h: 900 },
+    "4/3": { ratio: 4 / 3, h: 1200 },
+    "1/1": { ratio: 1, h: 1600 },
+  };
+
+  const { ratio, h } = ratioMap[aspectRatio];
+  const optimizedUrl = `${image.url}?w=1600&h=${h}&fit=fill`;
+
   return (
-    <div className="section image">
+    <div
+      className="section image"
+      style={{
+        aspectRatio: aspectRatio,
+        position: "relative",
+        width: "100%",
+        maxWidth: "1600px",
+      }}
+    >
       <Image
-        src={image.url}
+        src={optimizedUrl}
         alt={image.fileName}
-        height={image.height}
-        width={image.width}
+        fill
         quality={80}
         blurDataURL={`data:image/png;base64,${imageBlur}`}
         placeholder="blur"
