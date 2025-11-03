915c0fc84c6873ed91d302455e9b57b53c77e997
David Riches <hi@davidrich.es>
2025-11-03 11:49:42 +0000

    updating font loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
 import "../styles/global.scss";
 import { ThemeProvider } from 'next-themes'
 import Layout from "../components/Layout";
+import { Karla, Oswald } from 'next/font/google';
+
+const karla = Karla({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-karla',
+});
+
+const oswald = Oswald({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-oswald',
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <style jsx global>{`
+        :root {
+          --font-karla: ${karla.style.fontFamily};
+          --font-oswald: ${oswald.style.fontFamily};
+        }
+      `}</style>
       <Layout>
         <Component {...pageProps} />
       </Layout>
