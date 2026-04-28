32c958e600f55c111ceecf8fa1de2f054d195f5d
David Riches <hi@davidrich.es>
2026-04-28 22:33:25 +0100

    updatez

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Header from "./Header";
 import Footer from "./Footer";
 import type { LayoutProps } from "@/types/components";
 import { useScrollAnimation } from "@/hooks/useScrollAnimation";
 
 export default function Layout({ children }: LayoutProps) {
+  const pathname = usePathname();
+
   useScrollAnimation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
 
   return (
     <div className="container-wrap">
